8a762c09e0dfd6a5d5245204993b2b4e56ee94c6
renkaifei <18910891894@163.com>
2021-01-30 21:06:12 +0800

    更新数据库文件

---
+++ StockWorm/StockWorm.Repository/Database/security.sql
@@ -0,0 +1,39 @@
+create table if not exists Security
+(
+	SecurityID integer not null primary key AUTOINCREMENT,
+    SecurityCode varchar(10) not null,
+    SecurityAbbr varchar(128) not null,
+    CompanyName varchar(128) not null,
+    CompanyAbbr varchar(128) not null,
+    ListingData DateTime not null,
+	ExchangeMarket varchar(10) not null
+);
+
+create table if not exists SecurityTask(
+	TaskID integer not null primary key AUTOINCREMENT,
+	SecurityCode varchar(10) not null,
+	ExchangeMarket varchar(10) not null,
+	BeginDate DateTime not null,
+	EndDate DateTime not null,
+	IsFinished int not null
+);
+
+create table if not exists SecurityDayQuotation
+(
+	id integer not null primary key AUTOINCREMENT,
+	SecurityCode varchar(10) not null,
+	TxDate DateTime not null,
+	ClosePrice float not null,
+	HighPrice float not null,
+	LowPrice float not null,
+	OpenPrice float not null,
+	LastClosePrice float not null,
+	PriceChange float not null,
+	Change float not null,
+	TurnOver float not null,
+	VolumeTurnOver float not null,
+	PriceTurnOver float not null,
+	MarketValue float not null,
+	NegoValue float not null
+);
+
